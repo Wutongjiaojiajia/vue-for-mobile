bffdafca7d6fb81ecc161c9c79ce5aec31a46ce2
黄家豪 <490899577@qq.com>
2020-06-26 10:26:20 +0800

    更新页面

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';  //引入视图router-view
+import router from './router';  //引入路由
+import store from './store';  //
+
+import './components';  // 扫描全局对象并自动注册 components -> global
+
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,26 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import routes from './routeCheck';
+Vue.use(VueRouter);
 
-Vue.use(VueRouter)
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('../views/Home.vue'),
+    meta: { title: '首页', keepAlive: false }
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/About.vue'),
+    meta: { title: '其他', keepAlive: false }
+  }
+]
 
 
 const router = new VueRouter({
-  routes
+  routes,
+  scrollBehavior: () => ({ y: 0 })
 })
 
 // 路由切换更改标题
